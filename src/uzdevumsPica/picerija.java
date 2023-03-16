d4e59fd1a2a4eecea07e898652c85c1b4b7bdbae
Volters <kevinsvolters12@gmail.com>
2023-03-16 10:10:02 +0200

    parveidoju abus arraylist uz vienu hashmap, lai klients var pasūtīt vairākas picas, kā arī pielaboju dažas kļūdas

--- src/uzdevumsPica/picerija.java
+++ src/uzdevumsPica/picerija.java
@@ -1,5 +1,6 @@
 package uzdevumsPica;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 import javax.swing.JOptionPane;
 
@@ -10,8 +11,8 @@ public class Picerija {
 	public static void main(String[] args) {
 		String[] darbibas = {"Pievienot pasûtîjumu", "Apskatît pasûtîjumus", "Apturçt programmu"};
 		String izvele;
-		ArrayList<Klients> klientuArrayList = new ArrayList<Klients>();
-		ArrayList<Pica> picuArrayList = new ArrayList<Pica>();
+		HashMap<Klients, ArrayList<Pica>> picuHM = new HashMap<Klients, ArrayList<Pica>>();
+		int picuSk;
 		
 		do{
 			izvele = (String)JOptionPane.showInputDialog(null, "Izvçlies darbîbu", "Izvçle", JOptionPane.QUESTION_MESSAGE, null, darbibas, darbibas[0]);
@@ -28,8 +29,11 @@ public class Picerija {
 				
 				//klientu datu ievade
 					vards = JOptionPane.showInputDialog("Kâ sauc klientu?");
-					talrunis = JOptionPane.showInputDialog("Kâds ir klienta tâlruòa nr?");
-					//piegade ja ne
+					
+					do{
+						talrunis = JOptionPane.showInputDialog("Kâds ir klienta tâlruòa nummurs?");
+					}while(talrunis.length()!=8);
+					
 					int yesno = JOptionPane.showOptionDialog(null, "Vai nepiecieðama piegâde? (3 EUR)", "Piegâde", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE, null, piegadesIzveles, piegadesIzveles[0]);
 					//ja ir piegade tad prasa adresi, ja nav tad uz vietas
 					if(yesno == 0){
@@ -42,71 +46,84 @@ public class Picerija {
 					
 					
 					//pievieno izveidoto klientu arraylistam
-					klientuArrayList.add(new Klients(vards, adrese, talrunis));
+					Klients klients = new Klients(vards, adrese, talrunis);
+					picuHM.put(klients, new ArrayList<Pica>());
 				
 				
 				JOptionPane.showMessageDialog(null, "Klients reìistrçts! Tagad pierakstam viòa pasûtîjumu!", "Informâcija", JOptionPane.INFORMATION_MESSAGE);
 				
-	
+				//picu skaits
+				picuSk = Integer.parseInt(JOptionPane.showInputDialog("Ievadi picu skaitu"));	
+				for(int i=0; i<picuSk; i++){
 					//izmeru izvele
 					izmers = (String)JOptionPane.showInputDialog(null, "Izvçlies izmçru"
-							+ "\nS - 4.99EUR"
-							+"\nM - 9.99EUR"
-							+"\nL - 14.99EUR"
-							+"\nXL - 20.00EUR", "Pica", JOptionPane.QUESTION_MESSAGE, null, izmeruIzvele, izmeruIzvele[0]);
+							+ "\nS - 4.99 EUR"
+							+"\nM - 9.99 EUR"
+							+"\nL - 14.99 EUR"
+							+"\nXL - 20.00 EUR", (i+1)+". pica", JOptionPane.QUESTION_MESSAGE, null, izmeruIzvele, izmeruIzvele[0]);
 					
 					//piedevu skaits
 					int piedevuSk;
 					do{
-						piedevuSk = Integer.parseInt(JOptionPane.showInputDialog(null, "Cik piedevas? (1.50 EUR)", "Pica", JOptionPane.QUESTION_MESSAGE));
+						piedevuSk = Integer.parseInt(JOptionPane.showInputDialog(null, "Cik piedevas? (1.50 EUR)", (i+1)+". pica", JOptionPane.QUESTION_MESSAGE));
 					}while(piedevuSk<0 || piedevuSk>5);
 					
 					
 					//piedevu izvele
 					String[] piedevas = new String[piedevuSk];
 					for(int j=0; j<piedevuSk; j++){
-						piedevas[j] = (String)JOptionPane.showInputDialog(null, "Izvçlies "+(j+1)+". piedevu", "Pica", JOptionPane.QUESTION_MESSAGE, null, piedevuIzvele, piedevuIzvele[0]);
+						piedevas[j] = (String)JOptionPane.showInputDialog(null, "Izvçlies "+(j+1)+". piedevu", (i+1)+". pica", JOptionPane.QUESTION_MESSAGE, null, piedevuIzvele, piedevuIzvele[0]);
 					}
 					
 					//mercu skaits
 					int mercuSk;
 					do{
-						mercuSk = Integer.parseInt(JOptionPane.showInputDialog(null, "Cik mçrces? (0.90 EUR)", "Pica", JOptionPane.QUESTION_MESSAGE));
+						mercuSk = Integer.parseInt(JOptionPane.showInputDialog(null, "Cik mçrces? (0.90 EUR)", (i+1)+". pica", JOptionPane.QUESTION_MESSAGE));
 					}while(mercuSk<0 || mercuSk>5);
 					
 					//mercu izvele
 					String[] merces = new String[mercuSk];
 					for(int j=0; j<mercuSk; j++){
-						merces[j] = (String)JOptionPane.showInputDialog(null, "Izvçlies "+(j+1)+". mçrci", "Pica", JOptionPane.QUESTION_MESSAGE, null, mercuIzvele, mercuIzvele[0]);
+						merces[j] = (String)JOptionPane.showInputDialog(null, "Izvçlies "+(j+1)+". mçrci", (i+1)+". pica", JOptionPane.QUESTION_MESSAGE, null, mercuIzvele, mercuIzvele[0]);
 					}
 					
 					
 					
 					//pievieno izveidoto picu arraylistam
-					picuArrayList.add(new Pica(izmers, piedevuSk, piedevas, mercuSk, merces, piegade));
+					picuHM.get(klients).add(new Pica(izmers, piedevuSk, piedevas, mercuSk, merces, piegade ));
 					JOptionPane.showMessageDialog(null, "Pica reìistrçta!", "Informâcija", JOptionPane.INFORMATION_MESSAGE);
+				}
+					
 				break;
 				
 			case "Apskatît pasûtîjumus":
-				if(klientuArrayList.size()<1){
+				if(picuHM.isEmpty()){
 					JOptionPane.showMessageDialog(null, "Nav reìistrçtu klientu!", "Íïûda!", JOptionPane.ERROR_MESSAGE);
 				}else{
-					for(int i=0; i<klientuArrayList.size(); i++){
-						JOptionPane.showMessageDialog(null, "Informâcija par "+(i+1)+". klientu:"
-								+ "\nVârds: "+klientuArrayList.get(i).getVards()
-								+ "\nTâlrunis: "+klientuArrayList.get(i).getTalrunis()
-								+ "\nAdrese: "+klientuArrayList.get(i).getAdrese()
-								+ "\nIzmçrs: "+picuArrayList.get(i).getIzmers()
-								+ "\nPiedevas: "+picuArrayList.get(i).getPiedevas()
-								+ "\nMçrces: "+picuArrayList.get(i).getMerces()
-								+ "\nCena: "+picuArrayList.get(i).aprekinatCenu());
-						
-										
-					}
+					//saliek klienta informaciju vienaa stringaa
+					for (Klients i : picuHM.keySet()) {
+						String output="";
+						 output+= 	"\nVârds: "+i.getVards()
+									+ "\nTâlrunis: "+i.getTalrunis()
+									+ "\nAdrese: "+i.getAdrese()
+									+ "\n**************************";
+						 ArrayList<Pica> picas = picuHM.get(i);
+						 
+						 for(int j=0; j<picas.size(); j++) {
+							 output+= 	"\n"+(j+1)+". pica"
+									 	+ "\nIzmçrs: "+picas.get(j).getIzmers()
+									 	+ "\nPiedevas: "+picas.get(j).getPiedevas()
+									 	+ "\nMçrces: "+picas.get(j).getMerces()
+									 	+ "\nCena: "+picas.get(j).aprekinatCenu()+" EUR"
+									 	+ "\n----------------------------------------";
+						 }
+						 
+						 JOptionPane.showMessageDialog(null, output, "Informâcija par "+i.getVards(), JOptionPane.INFORMATION_MESSAGE);
+						 output="";
+						}
+					
 				}
-				
-				
-				
+			
 				break;
 				
 			case "Apturçt programmu":
